3ebb8cb7b1c0f5706ce38844490d78ac8962c35b
Grace <wangxiaotian2012@gmail.com>
2022-09-10 16:17:54 -0700

    Complete Assignment 3: practice for built-in directives

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  clickLog: number[] = [];
+  showParagraph = true;
+
+  onClick(event) {
+    this.clickLog.push(event.timeStamp);
+    this.showParagraph = !this.showParagraph;
+    console.log('clickLog', this.clickLog);
+  }
+
+  getLocaleString(timestamp) {
+    return new Date(timestamp * 1000).toLocaleString();
+  }
 }
