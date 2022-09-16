57db4f8eba3fe8febcbf1a4140c351af1913fa0b
Grace <wangxiaotian2012@gmail.com>
2022-09-16 13:42:33 -0700

    Implement the event listener for the custom event

--- cmp-databinding-assignment-start/src/app/app.component.ts
+++ cmp-databinding-assignment-start/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIncrementNumber(gameData: { count: number }) {
+    const newNumber = gameData.count;
+    if (newNumber % 2 === 1) {
+      this.oddNumbers.push(newNumber);
+    } else {
+      this.evenNumbers.push(newNumber);
+    }
+
+    console.log("this.oddNumbers", this.oddNumbers);
+    console.log("this.evenNumbers", this.evenNumbers);
+  }
 }
