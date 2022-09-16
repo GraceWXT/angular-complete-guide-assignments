618fcd25d4c8d8b01d777f8ae36eb6c7d83bf665
Grace <wangxiaotian2012@gmail.com>
2022-09-16 11:49:17 -0700

    Implement the game control buttons and event listeners

--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"],
 })
 export class GameControlComponent implements OnInit {
+  @Output() incrementNumber = new EventEmitter<{ count: number }>();
+  intervalId: NodeJS.Timeout;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  startCount() {
+    let count = 0;
+    this.intervalId = setInterval(() => {
+      count++;
+      this.incrementNumber.emit({ count });
+      console.log("count", count);
+    }, 1000);
   }
 
+  stopCount() {
+    clearInterval(this.intervalId);
+  }
 }
