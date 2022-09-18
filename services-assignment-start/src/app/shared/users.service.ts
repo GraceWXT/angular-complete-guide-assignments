df1a54204cb79e39312161a3bde5a6631a821d47
Grace <wangxiaotian2012@gmail.com>
2022-09-17 19:53:03 -0700

    Inject the countersService into UsersService

--- services-assignment-start/src/app/shared/users.service.ts
+++ services-assignment-start/src/app/shared/users.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from "@angular/core";
+import { CountersService } from "./counters.service";
 
 @Injectable()
 export class UsersService {
   activeUsers = ["Max", "Anna"];
   inactiveUsers = ["Chris", "Manu"];
 
+  constructor(private countersService: CountersService) {}
+
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.countersService.incrementToInactiveCounts();
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.countersService.incrementToActiveCounts();
   }
 }
