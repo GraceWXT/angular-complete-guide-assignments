57c24bb83bd61b59154763c359ac04bd796f711b
Grace <wangxiaotian2012@gmail.com>
2022-09-26 20:34:16 -0700

    Correct the reactive form module provider

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [FormBuilder],
 })
 export class AppComponent implements OnInit {
   projectForm: FormGroup;
