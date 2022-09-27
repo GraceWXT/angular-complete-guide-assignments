26a22fccb36a9a48a56762efa15cf867efc7991b
Grace <wangxiaotian2012@gmail.com>
2022-09-26 20:02:30 -0700

    Create the ts form object with basic validators

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  projectStatusOptions: ["Stable", "Critical", "Finished"];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.projectForm = this.formBuilder.group({
+      projectName: ["", Validators.required],
+      mail: ["", Validators.required, Validators.email],
+      projectStatus: [""],
+    });
+  }
 }
